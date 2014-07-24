818fb5234dde8639bb78cf731d03e80a7e305eec
Chris Rittelmeyer <github.10.iamchris@spamgourmet.com>
2014-07-23 22:31:31 -0400

    First commit - Control a box on the screen with arrows and space bar to jump.

---
+++ FancyChance/Assets/Scripts/PlayerController.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+var gravity : float = 20;
+var speed : float = 8;
+var acceleration : float = 30;
+var jumpHeight : float = 12;
+
+private var currentSpeed : float;
+private var targetSpeed : float;
+private var amountToMove : Vector2;
+
+private var playerPhysics : PlayerPhysics;
+
+function Start () {
+	playerPhysics = GetComponent(PlayerPhysics);
+}
+
+function Update () {
+	targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+	currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
+	
+	if (playerPhysics.grounded) {
+	
+		amountToMove.y = 0;
+	
+		//Jump
+		if (Input.GetButtonDown("Jump")) {
+			amountToMove.y = jumpHeight;
+		}
+	}
+	
+	amountToMove.x = currentSpeed;
+	amountToMove.y -= gravity * Time.deltaTime;
+	playerPhysics.Move(amountToMove * Time.deltaTime);
+}
+
+function IncrementTowards(n : float, target : float, a : float) {
+	if (n == target) { 
+		return n;
+	} else {
+		var dir : float = Mathf.Sign(target - n); //n must be increased or decreased to get closer to target
+		n += a * Time.deltaTime * dir;
+		return (dir == Mathf.Sign(target - n)) ? n : target; //if n has now passed the target then return target, otherwise return n
+	}
+}
---
+++ FancyChance/Assets/Scripts/PlayerPhysics.js
@@ -0,0 +1,57 @@
+﻿#pragma strict
+
+var collisionMask : LayerMask;
+
+private var coll : BoxCollider;
+private var s : Vector3;
+private var c : Vector3;
+
+@HideInInspector
+var grounded : boolean;
+
+private var skin : float = 0.005f;
+
+var ray : Ray;
+var hit : RaycastHit;
+
+function Start() {
+	coll = GetComponent(BoxCollider);
+	s = coll.size;
+	c = coll.center;
+}
+
+function Move(moveAmount : Vector2) {
+	grounded = false;
+
+	var deltaY : float = moveAmount.y;
+	var deltaX : float = moveAmount.x;
+	var p : Vector2 = transform.position;
+	
+	for (var i = 0; i < 3; i++) {
+		var dir : float = Mathf.Sign(deltaY);
+		var x : float = (p.x + c.x - s.x/2) + s.x/2 * i; //left, center, and then right most point of collider
+		var y : float = p.y + c.y + s.y/2 * dir; //bottom of collider
+		
+		ray = new Ray(new Vector2(x, y), new Vector2(0, dir));
+		Debug.DrawRay(ray.origin, ray.direction);
+		
+		if (Physics.Raycast(ray, hit, Mathf.Abs(deltaY), collisionMask)) {
+			//Get distance between player and ground
+			var dst : float = Vector3.Distance(ray.origin, hit.point);
+			
+			//Stop player's downward movement after coming within skin width of a collider
+			if (dst > skin) {
+				deltaY = dst * dir + skin;
+			} else {
+				deltaY = 0;
+			}
+			
+			grounded = true;
+			break;
+		}
+	}
+	
+	var finalTransform = new Vector2(deltaX, deltaY);
+
+	transform.Translate(finalTransform);
+}
