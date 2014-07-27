cad6e0971a238f88ca55a1b19ca7fed136867aa0
Chris Rittelmeyer <crittelmeyer@floatingtree.com>
2014-07-27 15:04:59 -0400

    Added a simple model to replace our cube as the player. Model includes idle, walking, and running animations. Created a run button so the plater can walk or run. Made the player switch the direction it was facing when they moved in that direction.

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -1,29 +1,33 @@
 ﻿#pragma strict
 
 var gravity : float = 20;
-var speed : float = 8;
+var walkSpeed : float = 8;
+var runSpeed : float = 12;
 var acceleration : float = 30;
 var jumpHeight : float = 12;
 
+private var animationSpeed : float;
 private var currentSpeed : float;
 private var targetSpeed : float;
 private var amountToMove : Vector2;
 
 private var playerPhysics : PlayerPhysics;
+private var animator : Animator;
 
 function Start () {
 	playerPhysics = GetComponent(PlayerPhysics);
+	animator = GetComponent(Animator);
 }
 
 function Update () {
+
+	//reset acceleration upon collision
 	if (playerPhysics.movementStopped) {
 		targetSpeed = 0;
 		currentSpeed = 0;
 	}
-
-	targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
-	currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
 	
+	//if player is touching the ground
 	if (playerPhysics.grounded) {
 	
 		amountToMove.y = 0;
@@ -34,9 +38,23 @@ function Update () {
 		}
 	}
 	
+	animationSpeed = IncrementTowards(animationSpeed, Mathf.Abs(targetSpeed), acceleration);
+	animator.SetFloat("Speed", Mathf.Abs(animationSpeed));
+	
+	//process input
+	var speed : float = Input.GetButton("Run") ? runSpeed : walkSpeed;
+	targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+	currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
+	
+	//set amout to move
 	amountToMove.x = currentSpeed;
 	amountToMove.y -= gravity * Time.deltaTime;
 	playerPhysics.Move(amountToMove * Time.deltaTime);
+	
+	var moveDir : float = Input.GetAxisRaw("Horizontal");
+	if (moveDir != 0) {
+		transform.eulerAngles = moveDir > 0 ? Vector3.up * 180 : Vector3.zero;
+	}
 }
 
 function IncrementTowards(n : float, target : float, a : float) {
--- Assets/Scripts/PlayerPhysics.js
+++ Assets/Scripts/PlayerPhysics.js
@@ -6,6 +6,13 @@ private var coll : BoxCollider;
 private var s : Vector3;
 private var c : Vector3;
 
+private var originalSize : Vector3;
+private var originalCenter : Vector3;
+private var colliderScale : float;
+
+private var collisionDivisionsX : int = 3;
+private var collisionDivisionsY : int = 10;
+
 @HideInInspector
 var grounded : boolean;
 @HideInInspector
@@ -18,8 +25,11 @@ var hit : RaycastHit;
 
 function Start() {
 	coll = GetComponent(BoxCollider);
-	s = coll.size;
-	c = coll.center;
+	colliderScale = transform.localScale.x;
+	
+	originalSize = coll.size;
+	originalCenter = coll.center;
+	SetCollider(originalSize, originalCenter);
 }
 
 function Move(moveAmount : Vector2) {
@@ -36,9 +46,9 @@ function Move(moveAmount : Vector2) {
 	//check top and bottom collisions
 	grounded = false;
 	
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < collisionDivisionsX; i++) {
 		dir = Mathf.Sign(deltaY);
-		x = (p.x + c.x - s.x/2) + s.x/2 * i; //left, center, and then right most point of collider
+		x = (p.x + c.x - s.x/2) + s.x/(collisionDivisionsX - 1) * i; //left, center, and then right most point of collider
 		y = p.y + c.y + s.y/2 * dir; //bottom of collider
 		
 		ray = new Ray(new Vector2(x, y), new Vector2(0, dir));
@@ -62,10 +72,10 @@ function Move(moveAmount : Vector2) {
 	
 	//check left and right collisions
 	movementStopped = false;
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < collisionDivisionsY; i++) {
 		dir = Mathf.Sign(deltaX);
 		x = p.x + c.x + s.x/2 * dir; //right of collider
-		y = (p.y + c.y - s.y/2) + s.y/2 * i; //top, center, and then bottom most point of collider
+		y = (p.y + c.y - s.y/2) + s.y/(collisionDivisionsY - 1) * i; //top, center, and then bottom most point of collider
 		
 		ray = new Ray(new Vector2(x, y), new Vector2(dir, 0));
 		Debug.DrawRay(ray.origin, ray.direction);
@@ -86,7 +96,28 @@ function Move(moveAmount : Vector2) {
 		}
 	}
 	
+	//check collision in direction player is moving
+	if (!grounded && !movementStopped) {
+		var playerDir = Vector3(deltaX, deltaY);
+		var o = Vector3(p.x + c.x + s.x/2 * Mathf.Sign(deltaX), p.y + c.y + s.y/2 * Mathf.Sign(deltaY));
+		ray = new Ray(o, playerDir.normalized);
+		Debug.DrawRay(ray.origin, ray.direction);
+	
+		if (Physics.Raycast(ray, Mathf.Sqrt(deltaX * deltaX + deltaY * deltaY), collisionMask)) {
+			grounded = true;
+			deltaY = 0;
+		}
+	}
+	
 	var finalTransform = new Vector2(deltaX, deltaY);
 
-	transform.Translate(finalTransform);
+	transform.Translate(finalTransform, Space.World);
+}
+
+function SetCollider(size : Vector3, center : Vector3) {
+	coll.size = size;
+	coll.center = center;
+	
+	s = size * colliderScale;
+	c = center * colliderScale;
 }
